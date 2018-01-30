f7c97ddcdad8f53665ac2a53b1b8640cd29ddd56
AkashGutha <akash.gutha@outlook.com>
2018-01-30 21:55:33 +0530

    index.css is not a file in this repo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import 'tachyons-egghead'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
