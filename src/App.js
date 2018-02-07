e4c54eccc21defd16b85c9286ba0aa1120da8672
joelhooks <joelhooks@gmail.com>
2018-02-06 16:26:06 -0800

    use downshift for basic open/close functionality before adding search input

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ContentSelector from "./screens/ContentSelector"
 import selectedLessons from './data/selectedLessons'
 import allLessons from './data/lessons'
 import {DragDropContext, Draggable, Droppable} from 'react-beautiful-dnd'
+import Downshift from 'downshift'
 
 //replace this with a valid JWT to auth against the API as needed
 axios.defaults.headers.common['Authorization'] = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwidXNlcl91cmwiOiJodHRwOi8vZWdnaGVhZC5kZXY6NTAwMC9hcGkvdjEvdXNlcnMvY3VycmVudCIsImV4cCI6MTUwOTA1NDg1MiwibWV0YSI6eyJuYW1lIjoiSm9lbCIsImZpcnN0X25hbWUiOiJKb2VsIiwiZW1haWwiOiJqb2VsQGVnZ2hlYWQuaW8iLCJpZCI6MSwiY2FuX2NvbnRhY3QiOnRydWUsImlzX3BybyI6dHJ1ZSwiaXNfaW5zdHJ1Y3RvciI6dHJ1ZSwiY3VycmVudF9wbGFuIjoiYTZjNWM4MzYiLCJjcmVhdGVkX2F0IjoiMjAxMy0wOC0zMVQyMToyMToxMyIsInB1Ymxpc2hlZF9sZXNzb25zIjo3LCJpbnN0cnVjdG9yX2lkIjoiam9lbC1ob29rcyIsImNvbXBsZXRlZF9sZXNzb25zIjo1LCJsYXN0X2xlc3Nvbl93YXRjaGVkX2F0IjoiMjAxNy0wNC0yMFQxMzoyODozNiIsImxhc3RfbGVzc29uX3dhdGNoZWQiOiJqYXZhc2NyaXB0LXJlZHV4LWF2b2lkaW5nLWFycmF5LW11dGF0aW9ucy13aXRoLWNvbmNhdC1zbGljZS1hbmQtc3ByZWFkIiwibGFzdF9sZXNzb25fY29tcGxldGVkIjoiamF2YXNjcmlwdC1yZWR1eC1hdm9pZGluZy1hcnJheS1tdXRhdGlvbnMtd2l0aC1jb25jYXQtc2xpY2UtYW5kLXNwcmVhZCIsImxhc3RfbGVzc29uX2NvbXBsZXRlZF9hdCI6IjIwMTctMDQtMjBUMTM6Mjg6MzYiLCJ0b3RhbCI6MC4wLCJ0cmFuc2FjdGlvbnMiOjAsIm1lbWJlcl9zaW5jZSI6IjIwMTUtMTItMTRUMTA6MTQ6MDMiLCJpbnRlcnZhbCI6Im5ldmVyIiwicGxhbl9uYW1lIjoiTWFuYWdlZCBTdWJzY3JpcHRpb24ifX0.52WWyYmOIfJhZS-NW3xXWFQX51zOFnIb6eDkuPPi0IU';
@@ -14,7 +15,8 @@ axios.defaults.headers.common['Authorization'] = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGc
 class App extends Component {
   state = {
     selectedLessons,
-    allLessons
+    allLessons,
+    isOpen: false
   }
 
   removeItem = (item) => {
@@ -28,9 +30,14 @@ class App extends Component {
       this.removeItem(item)
     } else {
       this.setState({
-        selectedLessons: [item, ...this.state.selectedLessons]
+        selectedLessons: [item, ...this.state.selectedLessons],
+        isOpen: true
       })
     }
+
+  }
+
+  onToggleMenu = () => {
 
   }
 
@@ -86,7 +93,7 @@ class App extends Component {
                                 <div
                                   key={item.slug}
                                   {...dragProvided.draggableProps}
-                                     {...dragProvided.dragHandleProps}
+                                  {...dragProvided.dragHandleProps}
                                   {...getItemProps({
                                     onClick: () => this.removeItem(item),
                                     item
@@ -109,22 +116,51 @@ class App extends Component {
 
           </DragDropContext>
           <hr/>
-          <List render={({getItemProps}) => (
-            <div>
-              {
-                this.state.allLessons.map((item) => (
-                  <div {...getItemProps({
-                    onClick: () => this.selectItem(item),
-                    item,
-                    style: {fontWeight: some(this.state.selectedLessons, item) ? 'bold' : 'normal'}
-                  })}>
-                    {item.slug}
-                  </div>
-                ))
 
-              }
-            </div>
-          )}/>
+          <Downshift itemToString={itemToString} isOpen={this.state.isOpen}>
+            {({
+                getRootProps,
+                getInputProps,
+                getButtonProps,
+                getItemProps,
+                isOpen,
+                toggleMenu,
+                clearSelection,
+                selectedItem,
+                inputValue,
+                highlightedIndex,
+              }) => (
+              <div>
+                <button {...getButtonProps({onClick: () => this.setState({isOpen: !this.state.isOpen})})} >open</button>
+
+                {!isOpen
+                  ? null
+                  : <div>
+                    {
+                      this.state.allLessons.map((item) => (
+                        <div {...getItemProps({
+                          onClick: () => this.selectItem(item),
+                          item,
+                          style: {fontWeight: some(this.state.selectedLessons, item) ? 'bold' : 'normal'}
+                        })}>
+                          {item.slug}
+                        </div>
+                      ))
+
+                    }
+                  </div>}
+              </div>
+            )}
+          </Downshift>
+
+
+
+
+
+
+
+
+
 
         </div>
       </StaticRouter>
@@ -160,6 +196,10 @@ function composeEventHandlers(...fns) {
       fn && fn(event, ...args)
       return event.defaultPrevented
     })
+}
+
+function itemToString(item) {
+  return item.slug
 }
 
 class List extends React.Component {
